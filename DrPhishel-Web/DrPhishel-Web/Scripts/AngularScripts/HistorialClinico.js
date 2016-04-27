fe2bb69f55db8f53ad04a0a4d418df39a8a590e0
Cristian Rivera <crisrivlop@gmail.com>
2016-04-27 05:53:43 -0600

    primeras vistas agregadas

---
+++ DrPhishel-Web/DrPhishel-Web/Scripts/AngularScripts/HistorialClinico.js
@@ -0,0 +1,10 @@
+﻿
+myApp.controller('historialClinicoController', function ($scope, $http) {
+    $scope.citas = [{ DoctorEncargado: "Fernando Rivera", FechaCita: "Hoy", Descripcion: "nada", ResultadosDeEstudios: "no pos paciente gg" }, { DoctorEncargado: "Fernando Rivera", FechaCita: "ayer", Descripcion: "nope", ResultadosDeEstudios: "pos paciente gg" }];
+    $scope.receta = [{ nombre: "Medicamento a" }, { nombre: "Medicamento b" }];
+
+    $scope.seleccionarCita = function (cita) {
+        $scope.citaSeleccionada = cita;
+    }
+
+});
